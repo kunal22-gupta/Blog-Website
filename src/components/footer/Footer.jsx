392eb1a46baeb407123b0e027e8ab0d5212c8392
Kunal Gupta <kunal22.10.2002@gmail.com>
2023-11-04 22:08:31 +0530

    fix: fix the wrong import statement

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import Logo from "../../components"
+import {Logo} from "../../components"
 
 export default function Footer() {
     return (
