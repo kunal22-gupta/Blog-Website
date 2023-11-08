b36ddcd7dff15e418fb4165d39155f1b71ba7f00
Kunal Gupta <kunal22.10.2002@gmail.com>
2023-11-08 20:46:51 +0530

    create a PostCard component for blogs

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,21 @@
 import Header from "./header/Header";
-import Footer  from "./footer/Footer";
+import Footer from "./footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import { LogoutBtn } from "./header/LogoutBtn";
 import Button from "./Button";
 import Input from "./Input";
 import Select from "./Select";
+import PostCard from "./PostCard";
 
-export { Header, Footer, Container, Logo, LogoutBtn, Button, Input, Select };
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    Button,
+    Input,
+    Select,
+    PostCard,
+};
