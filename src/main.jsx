daef8a009ffe7b0e427a590e17f227d48f0cd29a
Kunal Gupta <kunal22.10.2002@gmail.com>
2023-11-08 18:29:07 +0530

    Set up a demo route in main.jsx to check other components

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import {Provider} from "react-redux"
-import store from "./store/store.js"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Provider } from "react-redux";
+import store from "./store/store.js";
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
+import { Header } from "./components/index.js";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Provider store={store}>
-    <App />
-    </Provider>
-  </React.StrictMode>,
-)
+const router = createBrowserRouter(
+    createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="" element={<Header/>}/>
+    </Route>
+    )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+        <Provider store={store}>
+            <RouterProvider router={router} />
+        </Provider>
+);
