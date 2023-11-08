e1cc3c734b75990f191247d18eff72e1463e5680
Kunal Gupta <kunal22.10.2002@gmail.com>
2023-11-08 18:28:02 +0530

    create a reusable Button component and export it from index.js

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import Footer  from "./footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import { LogoutBtn } from "./header/LogoutBtn";
+import Button from "./Button";
 
-export { Header, Footer, Container, Logo, LogoutBtn };
+export { Header, Footer, Container, Logo, LogoutBtn, Button };
